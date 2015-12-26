9f9aacbddaf682001bd968bac22aaa84758bb5b3
Brian Broll <brian.broll@gmail.com>
2015-12-26 11:17:31 -0600

    Added basic loss decorator. Fixes #154
    WIP Updated metamodel

---
+++ src/decorators/LossDecorator/EasyDAG/LossDecorator.EasyDAGWidget.js
@@ -0,0 +1,41 @@
+/*globals define, _, $*/
+/*jshint browser: true, camelcase: false*/
+
+/**
+ * @author brollb / https://github.com/brollb
+ */
+
+define([
+    'decorators/LayerDecorator/EasyDAG/LayerDecorator.EasyDAGWidget',
+    'css!./LossDecorator.EasyDAGWidget.css',
+    'd3'
+], function (
+    LayerDecorator
+) {
+
+    'use strict';
+
+    var LossDecorator,
+        DECORATOR_ID = 'LossDecorator',
+
+    LossDecorator = function (options) {
+        var opts = _.extend({}, options);
+
+        this.dense = this.dense || {width: 110, height: 50};
+        this.size = this.size || {width: 110, height: 150};
+        this.color = this.color || '#80de80';
+        this._nameY = 25;
+
+        LayerDecorator.call(this, options);
+    };
+
+    LossDecorator.prototype.DECORATORID = DECORATOR_ID;
+    _.extend(LossDecorator.prototype, LayerDecorator.prototype);
+
+    LossDecorator.prototype.setAttributes = function() {
+        LayerDecorator.prototype.setAttributes.call(this);
+        this.dense.width = this.size.width = 11 * this.name.length;
+    };
+
+    return LossDecorator;
+});
